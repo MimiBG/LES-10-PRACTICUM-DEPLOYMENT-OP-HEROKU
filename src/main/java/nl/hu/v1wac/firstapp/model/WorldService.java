5f56d24dd7cd8158a8c06d8a56900290bc597c5f
MimiBG <31969921+MimiBG@users.noreply.github.com>
2020-08-29 23:18:16 +0200

    Update WorldService.java

--- src/main/java/nl/hu/v1wac/firstapp/model/WorldService.java
+++ src/main/java/nl/hu/v1wac/firstapp/model/WorldService.java
@@ -94,7 +94,7 @@ public class WorldService {
 		if (c != null) {
 			resultaat = countryDAO.delete(c);
 		} else {
-			throw new IllegalArgumentException("Code bestaat niet!");
+			throw new IllegalArgumentException("Deze code bestaat niet!");
 		}
 		return resultaat;
 	}
